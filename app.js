c68c5cd953b1a141a981bd1fcd9e173c005b4fe0
MichiY3 <michiyoy@hotmail.com>
2020-10-26 20:08:45 +0900

    クラスの追加削除

--- app.js
+++ app.js
@@ -27,6 +27,52 @@ console.log(tako);
 let ika = document.querySelectorAll('#tako .ika');
 console.log(ika);
 
+// 親要素取得方法
+let first_ika = document.querySelector('.ika');
+console.log('子要素の用意');
+console.log(first_ika);
+console.log('親要素の取得')
+let ul = first_ika.parentElement;
+console.log(ul);
+
+// 子要素取得方法
+console.log('親要素の用意');
+let parent = document.querySelector('#hogehoge');
+console.log(parent);
+console.log('子要素の取得');
+// console.log(parent.children);
+
+
+// クラスの追加
+first_ika.classList.add('textblue');
+
+// 問題：id =hogehogeのリストの文字を、全てtextblueを追加して青に変える
+// var parent = document.querySelector(#hogehoge);
+// console.log(parent);
+// hogehoge.classList.add('textblue');
+
+var first_fuga = document.querySelector('.fuga');
+let hogehoge = first_fuga.parentElement;
+console.log(hogehoge);
+hogehoge.classList.add('textblue');
+
+// ↑ これじゃ無駄なので、正解は↓
+
+document.querySelector('#hogehoge').classList.add('textblue');
+
+// または
+
+var parenthoge = document.querySelector('#hogehoge');
+	for (let li of parenthoge.children){
+		li.classList.add('textgrey');
+	}
+
+// クラスの削除
+
+
+
+
+
 
 
 
